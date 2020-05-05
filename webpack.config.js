72d0b821ecf579c33d5f775738fffbfb408cfe68
kiki7792 <921970613@qq.com>
2020-05-05 14:35:35 +0800

    html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+/**
+ * HtmlWebpackPlugin 會在打包結束後, 自動生成一個html文件,
+ * 并把打包生成的js文件自動引入到這個html文件中
+ */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path') // node核心模塊
 
 module.exports = {
@@ -44,5 +49,10 @@ module.exports = {
   output: { // 打包輸出的路徑
     filename: 'bundle.js', // 打包後的文件名
     path: path.resolve(__dirname, 'dist') // 打包的文件所在的文件夾名稱, __dirname指webpack.config.js所在的文件夾路徑
-  }
+  },
+  // plugin可以在webpack運行到某個時刻的時候, 幫你做一些事情, 類似hook生命周期函數
+  plugins: [new HtmlWebpackPlugin({
+    // template: `./src/index.html`
+    template: `src/index.html`
+  })]
 }
